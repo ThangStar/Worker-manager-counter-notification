3328db5d1b993b30a135fa50cb6d32a92252a95b
NongVanThang <nguyenquocthinhit@gmail.com>
2023-04-25 16:35:14 +0700

    main

--- app/src/main/java/com/example/workermanagercounttime/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/workermanagercounttime/ui/screens/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.example.workermanagercounttime.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.workermanagercounttime.ui.model.HomeViewModel
@@ -12,11 +16,15 @@ import com.example.workermanagercounttime.ui.model.HomeViewModel
 fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel(),
 ) {
-    Column() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Button(onClick = {
             homeViewModel.onClick()
         }) {
-            Text(text = "StartDownload")
+            Text(text = "Start")
         }
     }
 }
